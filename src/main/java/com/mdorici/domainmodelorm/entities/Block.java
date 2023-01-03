7876e063b3e2ca68014047c7399376899ea30113
Helio <user00user00user00user00@gmail.com>
2023-01-03 22:00:20 +0000

    first commit

---
+++ src/main/java/com/mdorici/domainmodelorm/entities/Block.java
@@ -0,0 +1,69 @@
+package com.mdorici.domainmodelorm.entities;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_block")
+public class Block {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant startDate;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant endDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "activity_id")
+	private Activity activity;
+	
+	public Block() {
+	}
+
+	public Block(Long id, Instant startDate, Instant endDate) {
+		this.id = id;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Instant startDate) {
+		this.startDate = startDate;
+	}
+
+	public Instant getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Instant endDate) {
+		this.endDate = endDate;
+	}
+
+	public Activity getActivity() {
+		return activity;
+	}
+
+}
